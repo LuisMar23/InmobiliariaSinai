75f27647545d9f1d706c2886ff3b71a188fb7e70
LuisMar23 <lusho18fernando@gmail.com>
2025-11-01 14:11:14 -0400

    sdasd

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  //production: true,
-  //apiUrl: 'http://31.97.162.173:3302/apisinai',
-  production: false,
-  apiUrl: 'http://localhost:3301/apisinai', 
-  fileServer: 'http://localhost:3301',
+  production: true,
+  apiUrl: 'http://31.97.162.173:3302/apisinai',
+  // production: false,
+  // apiUrl: 'http://localhost:3301/apisinai', 
+  // fileServer: 'http://localhost:3301',
 };
