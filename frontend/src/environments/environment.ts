0409847bfbe116b2ea0691bb47fb7becac4ebac9
Simon Llano <simonllano73@gmail.com>
2026-01-02 02:01:09 -0400

    Bs.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
    production: true,
-  apiUrl: 'http://157.245.165.123:3301/apisinai',
-  fileServer: 'http://157.245.165.123:3301',
-  // apiUrl: 'http://localhost:3301/apisinai',
-  //  fileServer: 'http://localhost:3301',
+  //apiUrl: 'http://157.245.165.123:3301/apisinai',
+  //fileServer: 'http://157.245.165.123:3301',
+  apiUrl: 'http://localhost:3301/apisinai',
+  fileServer: 'http://localhost:3301',
 };
