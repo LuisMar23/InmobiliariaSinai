b31d5fd6a5b12b5437ddfed8ce40e32bc57b0667
LuisMar23 <lusho18fernando@gmail.com>
2026-05-20 23:27:25 -0400

    cambios

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   // apiUrl: 'http://157.245.165.123:3301/apisinai',
   // fileServer: 'http://157.245.165.123:3301',
-  // apiUrl: 'http://localhost:3301/apisinai',
-  // fileServer: 'http://localhost:3301',
+  apiUrl: 'http://localhost:3301/apisinai',
+  fileServer: 'http://localhost:3301',
 
-  apiUrl: 'https://inmobiliriasinai.com/apisinai',
-  fileServer: 'https://inmobiliriasinai.com',
+  // apiUrl: 'https://inmobiliriasinai.com/apisinai',
+  // fileServer: 'https://inmobiliriasinai.com',
 };
